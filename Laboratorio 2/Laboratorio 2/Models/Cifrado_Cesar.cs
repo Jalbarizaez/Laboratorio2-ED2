e550855d32833337cbfde8b26aa6d2776605fa0b
Jalbarizaez <jalbarizaez@gmail.com>
2019-10-04 16:19:50 -0600

    Cifrado y descifrado cesar

---
+++ Laboratorio 2/Laboratorio 2/Models/Cifrado_Cesar.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Laboratorio_2.Models
+{
+    public class Cifrado_Cesar
+    {
+        private static Dictionary<char, char> Tabla_Caracteres = new Dictionary<char, char>();
+        private List<char> Creacion_clave = new List<char>();
+        private List<char> Abecedario = new List<char>();
+        private const int bufferlength = 750;
+        public void Cifrar(string path_archivo, string path_texto, string clave, string path_Escritura)
+        {
+            Crear_diccionario(clave, path_archivo);
+            Escribir_Cifrado(path_texto, path_Escritura);
+        }
+        private void Escribir_Cifrado(string path_texto, string path_Escritura)
+        {
+            var escritura = new char[bufferlength];
+            int i = 0;
+            var buffer = new char[bufferlength];
+            using (var writer = new FileStream(path_Escritura, FileMode.Append))
+            {
+                using (var file = new FileStream(path_texto, FileMode.Open))
+                {
+                    using (var reader = new BinaryReader(file))
+                    {
+
+                        while (reader.BaseStream.Position != reader.BaseStream.Length)
+                        {
+                            buffer = reader.ReadChars(bufferlength);
+                            foreach (var item in buffer)
+                            {
+                                if (Tabla_Caracteres.ContainsKey(item))
+                                {
+                                    escritura[i] = (Tabla_Caracteres[item]);
+                                    i++;
+                                }
+                                else
+                                {
+                                    escritura[i] = item;
+                                    i++;
+                                }
+                            }
+                            i = 0;
+                            var escritor = Encoding.UTF8.GetBytes(escritura);
+                            writer.Write(escritor, 0, escritor.Length);
+                            escritura = new char[bufferlength];
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private void Crear_diccionario(string clave, string path_archivo)
+        {
+
+            var buffer = new char[bufferlength];
+            //Se llena el diccionario con los valores iniciales
+            using (var file = new FileStream(path_archivo, FileMode.Open))
+            {
+                using (var reader = new BinaryReader(file))
+                {
+                    while (reader.BaseStream.Position != reader.BaseStream.Length)
+                    {
+                        buffer = reader.ReadChars(bufferlength);
+                        foreach (var item in buffer)
+                        {
+                            Abecedario.Add(item);
+
+                        }
+                    }
+                }
+
+            }
+            char[] Clave = clave.ToCharArray();
+            for (int i = 0; i < Clave.Length; i++)
+            {
+                if (!Creacion_clave.Contains(Clave[i]) && Abecedario.Contains(Clave[i]))
+                {
+                    Creacion_clave.Add(Clave[i]);
+                }
+            }
+            foreach (var item in Abecedario)
+            {
+                if (!Creacion_clave.Contains(item))
+                {
+                    Creacion_clave.Add(item);
+                }
+            }
+            var key = Abecedario.ToArray();
+            var value = Creacion_clave.ToArray();
+            for (int i = 0; i < Abecedario.Count; i++)
+            {
+                Tabla_Caracteres.Add(key[i], value[i]);
+            }
+
+        }
+
+    }
+}
