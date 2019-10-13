0cbfae5d4f052f7711b171005787dfb9167e8d1a
Srgio-0 <srgiokun@gmail.com>
2019-10-12 18:33:36 -0600

    Sbox

--- Laboratorio 2/Laboratorio 2/Models/SDES.cs
+++ Laboratorio 2/Laboratorio 2/Models/SDES.cs
@@ -6,19 +6,23 @@ using System.IO;
 
 namespace Laboratorio_2.Models
 {
-    public class SDES
-    {
-        private List<int> ip = new List<int>();
-        private List<int> ip_1 = new List<int>();
-        private List<int> ep = new List<int>();
-        private List<int> p10 = new List<int>();
-        private List<int> p8 = new List<int>();
-        private List<int> p4 = new List<int>();
-        private List<int> ls1 = new List<int>();
-        private List<int> ls2 = new List<int>();
+	public class SDES
+	{
+		private List<int> ip = new List<int>();
+		private List<int> ip_1 = new List<int>();
+		private List<int> ep = new List<int>();
+		private List<int> p10 = new List<int>();
+		private List<int> p8 = new List<int>();
+		private List<int> p4 = new List<int>();
+		private List<int> ls1 = new List<int>();
+		private List<int> ls2 = new List<int>();
+		private string[][] S0 = new string[4][];
+		private string[][] S1 = new string[4][];
+
         private void Read_Permutations(string path)
         {
-            string[] lines = File.ReadAllLines(path);
+
+			string[] lines = File.ReadAllLines(path);
             for (int i = 0; i < lines.Length; i++)
             {
                 string[] line_split = lines[i].Split(':');
@@ -80,6 +84,7 @@ namespace Laboratorio_2.Models
             keys[1] = last_permutation;
             return keys;
         }
+
         public void Cifrado(string bits, string path_read, string path_write)
         {
             Read_Permutations(path_read);
@@ -96,6 +101,7 @@ namespace Laboratorio_2.Models
             }
             return final;
         }
+
         private string[] LS1(string bits)
         {
             var bit = bits.ToCharArray();
@@ -112,6 +118,7 @@ namespace Laboratorio_2.Models
             final[1] += bit[5];
             return final;
         }
+
         private string P8(string bits)
         {
             var bit = bits.ToCharArray();
@@ -122,6 +129,7 @@ namespace Laboratorio_2.Models
             }
             return final;
         }
+
         private string[] LS2(string bits)
         {
             var bit = bits.ToCharArray();
@@ -140,5 +148,41 @@ namespace Laboratorio_2.Models
             final[1] += bit[6];
             return final;
         }
-    }
+
+		private string SBox(string bits)
+		{
+			S0[0] = new string[4] { "01", "00", "11", "10" };
+			S0[1] = new string[4] { "11", "10", "01", "00" };
+			S0[2] = new string[4] { "00", "10", "01", "11" };
+			S0[3] = new string[4] { "11", "01", "11", "10" };
+
+			S1[0] = new string[4] { "00", "01", "10", "11" };
+			S1[1] = new string[4] { "10", "00", "01", "11" };
+			S1[2] = new string[4] { "11", "00", "01", "00" };
+			S1[3] = new string[4] { "10", "01", "00", "11" };
+
+			char[] bit = bits.ToCharArray();
+
+			int F0 = IndiceSBox(bit[0].ToString() + bit[3].ToString());
+			int C0 = IndiceSBox(bit[1].ToString() + bit[2].ToString());
+			int F1 = IndiceSBox(bit[4].ToString() + bit[7].ToString());
+			int C1 = IndiceSBox(bit[5].ToString() + bit[6].ToString());
+
+			return S0[F0][C0] + S1[F1][C1];
+		}
+
+		private int IndiceSBox(string cadena)
+		{
+			if (cadena == "00")
+				return 0;
+			if (cadena == "01")
+				return 1;
+			if (cadena == "10")
+				return 2;
+			if (cadena == "11")
+				return 3;
+			else
+				return 0;
+		}
+	}
 }
